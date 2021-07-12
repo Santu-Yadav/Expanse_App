ac2bace21c85b14fe87ce61466c3158cb50479a0
Santu Yadav <santu.yadav2503@gmail.com>
2021-07-12 16:09:11 +0530

    Added Expanse app basic component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Expanses from "./Expanse/Expanse";
 
 function App() {
+  const Expanse = [
+    { title: "Car Insurance", amount: 294, date: new Date(2015, 5, 12) },
+    { title: "School    ", amount: 800, date: new Date(2016, 9, 29) },
+    { title: "Car Insurance", amount: 300, date: new Date(2021, 7, 1) },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Expanses
+        date={Expanse[0].date}
+        itenary={Expanse[0].title}
+        cost={Expanse[0].amount}
+      />
+      <Expanses
+        date={Expanse[1].date}
+        itenary={Expanse[1].title}
+        cost={Expanse[1].amount}
+      />
+      <Expanses
+        date={Expanse[2].date}
+        itenary={Expanse[2].title}
+        cost={Expanse[2].amount}
+      />
     </div>
   );
 }
